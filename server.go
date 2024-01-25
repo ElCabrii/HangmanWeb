cf812e1e5d3650f936b7e31cd5fe48549b4e26c0
dgabriel <gab.debure@gmail.com>
2024-01-25 13:45:42 +0100

    server update

--- server.go
+++ server.go
@@ -1,14 +1,25 @@
-package server
+package main
 
 import (
-	"fmt"
-	"log"
+	"html/template"
 	"net/http"
 )
 
-func main(){
-	fmt.Println("Starting server on port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
+type Page struct {
+	Valeur string
+}
+
+var templates = template.Must(template.ParseFiles("index.html"))
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	p := Page{Valeur: "webpage"}
+	err := templates.ExecuteTemplate(w, "index.html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
