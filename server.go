15f87176da864fa455b998661fa7eb66626befb6
dgabriel <gab.debure@gmail.com>
2024-01-29 14:46:04 +0100

    updated server.go and html files

--- server.go
+++ server.go
@@ -13,15 +13,21 @@ type Page struct {
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("public"))))
+	http.HandleFunc("/play", play)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func play(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "play.html", nil)
 }
