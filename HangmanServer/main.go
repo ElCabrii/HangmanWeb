4777b0bc18d613a5d485a5fcffde19a1d046cfe1
dgabriel <gab.debure@gmail.com>
2024-02-03 14:10:45 +0100

    server main.go updated with new player and game type

--- HangmanServer/main.go
+++ HangmanServer/main.go
@@ -9,7 +9,8 @@ func main() {
 	fmt.Printf("Server started at http://localhost:8080\n")
 	player := Player{Username: "Joueur", Score: 0}
 	http.HandleFunc("/", player.index)
-	http.HandleFunc("/play", play)
+	game := Game{Difficulty: 0, WordToGuess: "temp", Game: "temp"}
+	http.HandleFunc("/play", game.play)
 	handleDir()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
