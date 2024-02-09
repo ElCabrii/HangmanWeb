fb09da461648e86b63733a9465b972d3166c3de1
dgabriel <gabriel.debure@ynov.com>
2024-02-09 15:03:51 +0100

    fixing packages and import for VSCode

--- HangmanServer/main.go
+++ HangmanServer/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"Hangman-Web/HangmanServer/Serverfunc"
 	"fmt"
 	"net/http"
 )
 
 func main() {
 	fmt.Printf("Server started at http://localhost:8080\n")
-	http.HandleFunc("/", index)
-	game := Game{Difficulty: 0, WordToGuess: "temp", Game: []string{"temp"}, WrongLetters: "", Mistakes: 0, GameOver: 0}
-	http.HandleFunc("/play", game.play)
-	handleDir()
+	http.HandleFunc("/", Server.Index)
+	game := Server.Game{Difficulty: 0, WordToGuess: "temp", Game: []string{"temp"}, WrongLetters: "", Mistakes: 0, GameOver: 0}
+	http.HandleFunc("/play", game.Play)
+	Server.HandleDir()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
