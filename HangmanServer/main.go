fea56d4d70ee276727924aa9ea9a1866b3edf656
dgabriel <gabriel.debure@ynov.com>
2024-02-09 15:32:39 +0100

    commiting everything for pull

--- HangmanServer/main.go
+++ HangmanServer/main.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Printf("Server started at http://localhost:8080\n")
+	fmt.Printf("Server starting at http://localhost:8080\n")
 	http.HandleFunc("/", Server.Index)
 	game := Server.Game{Difficulty: 0, WordToGuess: "temp", Game: []string{"temp"}, WrongLetters: "", Mistakes: 0, GameOver: 0}
 	http.HandleFunc("/play", game.Play)
 	Server.HandleDir()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		return
+		fmt.Printf("Server failed to start\n")
+	} else {
+		fmt.Printf("Server started successfully\n")
 	}
 }
